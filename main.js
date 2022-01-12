d0567f00cf8ca0f588bc37b9a443ce99f26b39ad
meriem <elounimeriem@gmail.com>
2022-01-12 11:27:45 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+// console.log("Hello from Javascript")
+
+
+// creating an element
+var btn = document.createElement('button');
+btn.innerHTML = 'add';
+document.body.appendChild(btn);
+
+// change the content of the button
+var prg = document.querySelector("button").innerHTML = "delete";
+
+// adding id attribute
+var btn = document.querySelector("button");
+btn.setAttribute('id', 'myBtn');
+console.log(btn)
+
+// changing the id value
+btn.id = 'bouton';
+
+// adding style to the h3
+var heading = document.querySelector(".card > h3");
+heading.style.color = 'blue'
+heading.style.fontSize = '40px'
+
+// remove an element 
+var para = document.getElementById("paragraph")
+para.remove()
+
+// remove child element
+var el = document.getElementById("container").lastElementChild;
+el.parentNode.removeChild(el)
+
+// replace child element
+var list = document.querySelector('ul')
+var childList = document.createElement('li')
+childList.textContent = "first list";
+list.replaceChild(childList, list.childNodes[0])
+
+
+
+btn.addEventListener('click', function(){
+    
+})
